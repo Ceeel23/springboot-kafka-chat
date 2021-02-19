087e835c4ddf096aa6d6d4e3a1a03d581eaea74c
eclee23 <ec.lee2@cj.net>
2021-02-19 11:59:28 +0900

    Init Project Src

---
+++ src/main/java/com/ceeel/springbootkafkachat/SpringbootKafkaChatApplication.java
@@ -0,0 +1,13 @@
+package com.ceeel.springbootkafkachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceeel/springbootkafkachat/SpringbootKafkaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceeel.springbootkafkachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
